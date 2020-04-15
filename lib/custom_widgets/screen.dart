18c88c5d52a16a101d13cc205b929f532360328b
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-04-16 02:49:58 +0530

    reusable screen

---
+++ lib/custom_widgets/screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class ReusableScreen extends StatelessWidget{
+
+  final List<Widget> content;
+  final String appBarTitle;
+  final String propic;
+  final String username;
+  final String jobtitle;
+
+  ReusableScreen({
+    @required this.content, 
+    @required this.appBarTitle, 
+    @required this.propic, 
+    @required this.username, 
+    @required this.jobtitle
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(appBarTitle),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850]
+        ),
+        backgroundColor: Colors.grey[900],
+
+        drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/$propic'),
+                          radius: 35.0,
+                        ),
+                      ),
+                      Text('$username', style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                      Text('$jobtitle', style: TextStyle(color: Colors.cyan, fontSize: 18.0)),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[850],
+                  ),
+                ),
+                ListTile(
+                  title: Text('Calendar & Events'),
+                  trailing: Icon(Icons.calendar_today,color: Colors.cyan,),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Staff'),
+                  trailing: Icon(Icons.people, color: Colors.cyan,),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Sections'),
+                  trailing: Icon(Icons.map, color: Colors.cyan,),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Restaurent'),
+                  trailing: Icon(Icons.restaurant, color: Colors.cyan,),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.all(90.0),
+                  title: Text('Log out'),
+                  leading: Icon(Icons.computer, color: Colors.cyan,),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+
+        body: Container(
+          height: 800.0,
+          width: 600.0,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background2.jpg"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child : SingleChildScrollView(
+                  child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                //TODO : axis alignment affects, replace the children with child param
+                children: content,
+              ),
+            ),
+        ),
+      )
+      );
+  }
+
+}
