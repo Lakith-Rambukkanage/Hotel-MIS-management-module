ae98d80b1bcc669e0deaa2294c0d9662d09fd830
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-04-16 02:48:34 +0530

    minor cahnges

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/pages/management.dart';
 import 'package:flutter_learn/pages/section.dart';
+import 'package:flutter_learn/pages/staff.dart';
 import 'pages/home.dart';
 import 'pages/loading.dart';
 import 'pages/login.dart';
 import 'pages/test.dart';
 
 void main() => runApp(MaterialApp(
-  //home: Home(),
-  home: Section(),
+  home: Management(),
   routes: {
     //'/' : (context) => Loading(),
     '/home' : (context) => Home(),
     '/loading' : (context) => Loading(),
     '/login' : (context) => Login(),
+    '/sections' : (context) => Section(),
+    '/staff' : (context) => Staff(),
+    '/test' : (context) => Test(),
     //'/home' : (context) => Home(),
     //front end done : Loading, Login, 
     //add section, staff, event, calendar, profile, settings, notification etc.
