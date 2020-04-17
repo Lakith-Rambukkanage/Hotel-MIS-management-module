e7f827898e6ff263402e8eb3ba48953f94dd5a9a
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-04-18 00:48:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/pages/calendar.dart';
 import 'package:flutter_learn/pages/management.dart';
+import 'package:flutter_learn/pages/restaurant.dart';
 import 'package:flutter_learn/pages/section.dart';
 import 'package:flutter_learn/pages/staff.dart';
 import 'pages/home.dart';
@@ -8,7 +10,8 @@ import 'pages/login.dart';
 import 'pages/test.dart';
 
 void main() => runApp(MaterialApp(
-  home: Management(),
+  home: Home(),
+  initialRoute: '/home',
   routes: {
     //'/' : (context) => Loading(),
     '/home' : (context) => Home(),
@@ -16,6 +19,9 @@ void main() => runApp(MaterialApp(
     '/login' : (context) => Login(),
     '/sections' : (context) => Section(),
     '/staff' : (context) => Staff(),
+    '/calendar' : (context) => Calendar(),
+    '/restaurant' : (context) => Restaurant(),
+    '/staff/management' : (context) => Management(),
     '/test' : (context) => Test(),
     //'/home' : (context) => Home(),
     //front end done : Loading, Login, 
