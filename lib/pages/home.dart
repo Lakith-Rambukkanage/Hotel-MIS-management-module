afb8dd4be9d442d67b3ba354ec6e7ad38c482acb
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-04-18 00:48:49 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,9 @@ import 'package:flutter_learn/custom_widgets/roompanel.dart';
 import 'package:flutter_learn/custom_widgets/screen.dart';
 import 'package:flutter_learn/custom_widgets/staffpanel.dart';
 import 'package:flutter_learn/custom_widgets/todaypanel.dart';
+import 'rooms.dart';
+import 'section.dart';
+import 'staff.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -16,23 +19,46 @@ class Home extends StatelessWidget {
       content: <Widget>[
                 TodayPanel(),
                 SizedBox(height: 20.0,),
-                StaffPanel(
-                  staff: 'Staff',
-                  active : 100,
-                  leave : 40,
-                  awol : 2,
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(context,new MaterialPageRoute(
+                    builder: (context)=>
+                    new Staff())
+                    );
+                  },
+                  child: StaffPanel(
+                    staff: 'Staff',
+                    active : 100,
+                    leave : 40,
+                    awol : 2,
+                  ),
                 ),
                 SizedBox(height: 20.0,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    RoomPanel(
-                    available: 50, 
-                    cleaning: 20,
-                    checked: 150,
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.push(context,new MaterialPageRoute(
+                        builder: (context)=>
+                        new Rooms())
+                        );
+                      },
+                      child: RoomPanel(
+                        available: 50, 
+                        cleaning: 20,
+                        checked: 150,
+                      ),
                     ),
                     SizedBox(width: 10.0,),
-                    MapIcon(),
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.push(context,new MaterialPageRoute(
+                        builder: (context)=>
+                        new Section())
+                        );
+                      },
+                      child: MapIcon()),
                   ],
                 ),
                 SizedBox(height: 20.0,),
