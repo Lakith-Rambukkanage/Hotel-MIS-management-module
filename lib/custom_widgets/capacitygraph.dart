5fd4ddfd500d0a95d804b4c5889b296f48894574
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-04-18 00:46:03 +0530

    custom widget

---
+++ lib/custom_widgets/capacitygraph.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CapacityGraph extends StatelessWidget {
+  final int white;
+  final int cyan;
+  final int blue;
+  final double width;
+  final double height;
+  final String whiteLabel;
+  final String cyanLabel;
+  final String blueLabel;
+
+  const CapacityGraph({
+  @required this.white, 
+  @required this.cyan, 
+  @required this.blue,
+  @required this.whiteLabel, 
+  @required this.cyanLabel, 
+  @required this.blueLabel, 
+  @required this.width, 
+  @required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    int total = white + cyan + blue;
+    double whiteheight = 20 + height*white/total;
+    double cyanheight = 20 + height*cyan/total;
+    double blueheight = 20 + height*blue/total;
+    return Container(
+    //height: 200.0,
+    //width: 180.0,
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children : <Widget>[
+          Container(width: width,height: whiteheight, decoration:BoxDecoration(color: Colors.white,borderRadius: BorderRadius.vertical(top:Radius.circular(20) )), 
+            child: Center(child: Text(' $whiteLabel - $white')),),
+          Container(width: width,height: cyanheight, color: Colors.cyan, child: Center(child: Text(' $cyanLabel - $cyan')),),
+          Container(width: width,height: blueheight,decoration:BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.vertical(bottom:Radius.circular(20) )), 
+            child: Center(child: Text(' $blueLabel - $blue')),),
+          ],
+      ),
+    ),
+      );
+  }
+}
