ac70664fd992d68f23596f890724e2638391f729
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-05-23 20:34:40 +0530

    model refacor code

--- lib/models/manager.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_learn/models/user.dart';
-
-class Manager extends User{
-  int priviledgeLevel;
-  //add job titles
-  //accept registered users
-  //change priviledges
-  //remove users
-}
--- lib/models/receptionist.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_learn/models/employee.dart';
-
-class Receptionist extends Employee{
-  //TODO: Implement the ability to view rooms and room bookings
-}
