43e5b5902c983669ffd43de0199bdc9d142e69d8
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-05-23 20:35:13 +0530

    adjust for overflow

--- lib/custom_widgets/mapicon.dart
+++ lib/custom_widgets/mapicon.dart
@@ -7,20 +7,20 @@ class MapIcon extends StatelessWidget {
     return HomeTile(
       children: <Widget>[
                     Container(
-                        height: 170.0,
-                        width: 170.0,
+                        height: 100.0,
+                        width: 70.0,
                         child:  Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text('All Sections', style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                              child: Text('All Sections', style: TextStyle(color: Colors.white, fontSize: 10.0)),
                             ),
                             Container(
-                              height: 100.0,
-                              width: 120.0,
+                              height: 50.0,
+                              width: 60.0,
                               decoration: new BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(30.0)),
-                              child: Icon(Icons.map,color: Colors.grey[900],size: 80.0,)),
+                              child: Icon(Icons.map,color: Colors.grey[900],size: 30.0,)),
                             SizedBox(height: 15.0,)
                           ],
                         ),
